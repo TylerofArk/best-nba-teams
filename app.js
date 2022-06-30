88a2a52f66827e8c12d728f670da4c3ef0f6e788
Tyler Main <tyler.l.main@gmail.com>
2022-06-29 19:39:47 -0500

    added all js prompts

--- app.js
+++ app.js
@@ -1,58 +1,22 @@
-/* let yellow = "Yellow";
 
-let otherYellow = "yellow";
-
-let favColor = yellow;
-
-console.log(yellow);
-console.log(yellow); \
-
-console.log(yellow == other otherYellow);
-
-let usersColor = prompt("What is your favorite color?")
-console.log(usersColor)
-
-
-const favorite = Number(prompt("What is the mascot of your favorite NBA team?"));
-console.log(favorite);
-console.log(typeof favorite);
-
-if (favorite === Thunder) {
-    console.log('Cool! The Thunder are the best team!')
-} else {
-    console.log('Your team is bad.')
-}
-
-else if (favorite === 9) {
-    console.log('9 is also a cool number')
-} else {
-    console.log('Number is not 23 or 7 or 9')
-}
-
-if (favorite !== 23) console.log('Why not 23?');
-
-if (usersColor.toLowerCasae() == "yellow") {
-    console.log("Mine too!")
-}
-*/
 // Gets user input
 let name = prompt("What is your name?");
 let num = prompt("What is your favorite number? ");
 
 // Uses user input to print out information
-console.log("Hello " + "name" + "!");
+console.log("Hello " + name + "!");
 console.log(num + "?! That's my favorite number too!");
 
 
-let teamName = "Thunder";
+let answer = prompt('Who is your favorite NBA team?');
+let bestTeam = "thunder"
+let otherBestTeam = "Thunder"
 
-let otherTeamName = "thunder";
-
-let usersTeam = prompt("What is your favorite NBA team?")
-console.log(usersTeam)
-
-if (teamName.toLowerCase() == "thunder") {
+if (answer == bestTeam) {
     console.log("Thunder Up!")
-} else {
-    console.log("Your team is bad!")
+} else if
+    (answer.toLowerCase() == "thunder")
+    console.log("Thunder Up!")
+else {
+    console.log('Your team is bad!')
 }
